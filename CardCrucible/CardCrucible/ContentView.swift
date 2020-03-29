f3a07cad7828144fb34dac6d5cfa30f0736e3e16
Matthew Kressin <matthew.d.kressin@gmail.com>
2020-03-28 21:50:20 -0700

    starting project for ios game of (possibly multiple) card games

---
+++ CardCrucible/CardCrucible/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardCrucible
+//
+//  Created by Matthew Kressin on 3/28/20.
+//  Copyright © 2020 Matthew Kressin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
